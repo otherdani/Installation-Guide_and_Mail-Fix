e761dc9250ae5d12056e7b1f82e3faec7a440da3
ICarlota99 <k431999@gmail.com>
2024-12-09 16:19:11 -0500

    add login html structureand copyright

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from datetime import datetime
 
 # Configure application
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    return render_template("index.html", year=datetime.now().year)
+
+@app.route('/register')
+def register():
+    return render_template("login.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
