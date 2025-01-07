a76e0ca5a0328dd24c18225b56a658c71a88c26e
ICarlota99 <k431999@gmail.com>
2025-01-07 14:44:38 -0500

    add login backbone

--- app.py
+++ app.py
@@ -33,34 +33,43 @@ def register():
     return render_template(".html")
 
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
     # Forget any user_id
     session.clear()
 
-    # User reached route via POST
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username and password were submited
+        # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
-        
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-        
+            return error_message("must provide username", 403)
+
+        # Ensure password was submitted
+        if not request.form.get("password"):
+            return error_message("must provide password", 403)
+
         # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        print(rows)
 
         # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return error_message("invalid username and/or password", 403)
 
         # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
         return redirect("/")
-    
-    # User reached route via GET
-    else:
-        return render_template("login.html")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    return render_template("login.html")
     
 
 @app.route("/logout")
