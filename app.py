1f88e5867ffad4fe4b73b1ff0ba950f9e4f0286e
ICarlota99 <k431999@gmail.com>
2025-01-07 14:06:44 -0500

    add session config and decorator to ensure responses arent cached

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, make_response
+from flask_session import Session
 
 from helpers import error_message, login_required
 
 # Configure application
 app = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies?)
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/')
 def home():
+    """Display homepage"""
     # Verify if it is a new user
     if request.cookies.get("new_user") is None:
         # New user: show welcome message
@@ -18,12 +31,6 @@ def home():
         # Redirect to homepage
         return render_template("index.html", year=datetime.now().year)
 
-@app.route("/set-cookie")
-def set_cookie():
-    # Configurar la cookie para marcar que el usuario no es nuevo
-    response = make_response(redirect(url_for("index")))
-    response.set_cookie("new_user", "no", max_age=60*60*24*365)  # Cookie válida por 1 año
-    return response
 
 
 @app.route('/register', methods=["GET", "POST"])
