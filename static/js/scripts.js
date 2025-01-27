5ab3a23e805586d46639f93bd72480216a92e2a4
ICarlota99 <k431999@gmail.com>
2025-01-27 15:13:23 -0500

    minor changes

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,6 @@ const yearSpan = document.getElementById("current_year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
 
-// Use local timezone
-function formatDateForUser(dateString) {
-    var date = new Date(dateString);
-    return date.toLocaleString();
-}
-
 // Delete confirmation script
 function confirmDelete(event, url) {
     event.preventDefault(); 
